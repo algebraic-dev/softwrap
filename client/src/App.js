d7de98bbcce28a9fe1202ce86a8accd0ea55f862
chiyoku <chiyokuoni@gmail.com>
2021-04-21 09:21:53 -0300

    Add a simple hierarchy for react frontend

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          <code>
+            src/App.js
+          </code>
+          and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
