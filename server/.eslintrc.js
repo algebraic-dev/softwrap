fc0a163508cac1e48f145ee7f9da2d9bc38cce73
chiyoku <chiyokuoni@gmail.com>
2021-04-21 17:48:18 -0300

    Started crud server

---
+++ server/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
+};
---
+++ server/__tests__/crud.test.js
@@ -0,0 +1,3 @@
+describe('test of CRUD operations', () => {
+
+})
